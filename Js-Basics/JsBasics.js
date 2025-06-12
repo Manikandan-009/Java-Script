4a14637d345d9dd1c09930e711c7398b7c3314fc
Manikandan <manikandan131925@gmail.com>
2025-06-12 23:10:43 +0530

    JS Basics

---
+++ Js-Basics/JsBasics.js
@@ -0,0 +1,125 @@
+// console
+
+console.log("Hello World");
+
+// Variables
+
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+// Data Types
+
+var a = 10;
+var b = "Hello World";
+var c = true;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Operators
+
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a - b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a * b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a / b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a % b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a ** b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a++;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a--;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = ++a;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = --a;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a == b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a != b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a > b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a < b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a >= b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a <= b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a && b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a || b;
+console.log(c);
+
+// For loop
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// While loop
+
+var i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
